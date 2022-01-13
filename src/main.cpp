319d3494d4a43d161e5a5b59c28722181213ff2c
Gregory Sizer <gsizer@gmail.com>
2022-01-12 19:25:29 -0700

    touchpanel

--- src/main.cpp
+++ src/main.cpp
@@ -4,98 +4,97 @@
 ||  Template Project
 ******************************************************************************/
 #include <Arduino.h>
-// TouchPanel
-#include <Wire.h>
-#include <FT62XXTouchScreen.h>
 // Graphical Display
 #include <SPI.h>
 #include <TFT_eSPI.h>
-// Graphical Interface
-#include <lvgl.h>
+// Graphical User Interface
 #include "esp_freertos_hooks.h"
+#include <lvgl.h>
+// Touch Interface
+#include <Wire.h>
+#include <FT62XXTouchScreen.h>
 
-// Environment Variables
+// System Variables
 static const uint16_t ScreenWidth = TFT_WIDTH;
 static const uint16_t ScreenHeight = TFT_HEIGHT;
-static lv_obj_t *Screen;
 
-// DisplayDriver
+// Global Objects
 static TFT_eSPI Display = TFT_eSPI(ScreenWidth, ScreenHeight);
-static lv_disp_drv_t *lvDispDrv;
-static lv_disp_t *lvDisplay;
-static lv_disp_draw_buf_t lvDrawBuf;
-static lv_color_t lvBuffer[ScreenWidth*10];
+static lv_obj_t *HomeScreen;
+static FT62XXTouchScreen TouchPanel = FT62XXTouchScreen(ScreenHeight, PIN_SDA, PIN_SCL);
+static TouchPoint LastTouch;
 
-// Dummy InputDriver for LVGL
-static FT62XXTouchScreen TouchPanel = FT62XXTouchScreen(ScreenHeight, (uint8_t)PIN_SDA, (uint8_t)PIN_SCL);
-static lv_indev_drv_t lvIndevDrv;
-
-// callback for LVGL to flush display memory
-void cbFlushDisplay(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_ptr){
-  uint32_t w = ( area->x2 - area->x1 + 1 );
-  uint32_t h = ( area->y2 - area->y1 + 1 );
-  Display.startWrite();
-  Display.setAddrWindow( area->x1, area->y1, w, h );
-  Display.pushColors( (uint16_t *)&color_ptr->full, w * h, true );
-  Display.endWrite();
-  lv_disp_flush_ready(drv);
+static void cb_tick_task(void){
+  LV_ATTRIBUTE_TICK_INC(portTICK_RATE_MS);
 }
 
-// callback for LVGL to handle TouchPanel input
-void cbReadTouch(lv_indev_drv_t *driver, lv_indev_data_t *data){
-  TouchPoint point =TouchPanel.read();
-  if(point.touched){
-    data->state = LV_INDEV_STATE_PR;
-    data->point.x = point.xPos;
-    data->point.y = point.yPos;
-  }else{
-    data->state = LV_INDEV_STATE_REL;
+// HomeScreen event handler
+static void cbEventHandler(lv_event_t *event){
+  if(Serial.availableForWrite()){
+    Serial.println("You Touched Me.");
   }
 }
 
-// handle the buttons & fingers
-void HomeEventHandler(lv_event_t *event){}
-
-
-void setup() {
-  // open SerialPort
-  Serial.begin(115200);
-
-  // initialize Display using TFT_eSPI
-  Display.init();
-  Display.setRotation(1); // USB-C port to right-bottom
-  pinMode(TFT_BL, OUTPUT); // setup backlight
-  digitalWrite(TFT_BL, HIGH); // enable backlight
-
-  // initialize LVGL
-  lv_init();
-
-  // initialize DisplayDriver
-  lv_disp_drv_init(lvDispDrv);
-  lvDispDrv->hor_res = ScreenWidth;
-  lvDispDrv->ver_res = ScreenHeight;
-  lvDispDrv->flush_cb = cbFlushDisplay;
-  lvDispDrv->draw_buf = &lvDrawBuf;
-  lv_disp_drv_register(lvDispDrv);
-  lv_disp_draw_buf_init(&lvDrawBuf, lvBuffer, NULL, ScreenWidth*10);
-  lv_disp_set_default(lvDisplay);
-
-  // initialize TouchPanel
-  lv_indev_drv_init(&lvIndevDrv);
-  lvIndevDrv.type = LV_INDEV_TYPE_POINTER;
-  lvIndevDrv.read_cb = cbReadTouch;
-
-  // HomeScreen
-  Screen = lv_obj_create(NULL);
-  lv_obj_t *lblBackground = lv_label_create(Screen);
-  lv_label_set_text(lblBackground, "WT32-SC01-LVGL");
-  lv_obj_set_align(lblBackground, LV_ALIGN_CENTER);
-  lv_obj_add_event_cb(Screen, HomeEventHandler, LV_EVENT_CLICKED, NULL);
-  lv_scr_load(Screen);
+static void cbDisplayFlush(lv_disp_drv_t *displayDriver, const lv_area_t *area, lv_color_t *colorP){
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+    Display.startWrite();
+    Display.setAddrWindow(area->x1, area->y1, w, h);
+    Display.pushColors((uint16_t*)&colorP->full, w * h, true);
+    Display.endWrite();
+    lv_disp_flush_ready(displayDriver);
 }
 
-// let LVGL handle most things
-void loop() {
+static void cbTouchInput(lv_indev_drv_t *inputDriver, uint8_t event_id){
+  TouchPoint touchPoint = TouchPanel.read();
+  if(touchPoint.touched == true){
+    // do something with touch point information
+    LastTouch = touchPoint;
+  }
+  if(Serial.availableForWrite()){
+    Serial.printf("at %u,%u\n", LastTouch.xPos, LastTouch.yPos);
+  }
+}
+
+void setup(){
+  // attempt serial connection
+  Serial.begin(115200);
+  esp_err_t err = esp_register_freertos_tick_hook((esp_freertos_tick_cb_t)cb_tick_task);
+  // Hardware Display
+  Display.init();
+  Display.setRotation(1);
+  // Backlight
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+  // LVGL Display Driver
+  lv_init();
+  static lv_color_t buffer[ScreenWidth*10];
+  static lv_disp_draw_buf_t *lvDisplayBuffer;
+  static lv_disp_drv_t lvDisplayDriver;
+  lv_disp_draw_buf_init(lvDisplayBuffer, buffer, NULL, ScreenWidth*10);
+  lv_disp_drv_init(&lvDisplayDriver);
+  lvDisplayDriver.hor_res = 480;
+  lvDisplayDriver.ver_res = 320;
+  lvDisplayDriver.flush_cb = cbDisplayFlush;
+  lvDisplayDriver.draw_buf = lvDisplayBuffer;
+  lv_disp_drv_register(&lvDisplayDriver);
+  // Touch Panel
+  TouchPanel.begin();
+  static lv_indev_drv_t *lvInputDevice;
+  lv_indev_drv_init(lvInputDevice);
+  lvInputDevice->type = LV_INDEV_TYPE_POINTER;
+  lvInputDevice->feedback_cb = cbTouchInput;
+  lvInputDevice->disp = lv_disp_get_default();
+  lv_indev_drv_register(lvInputDevice);
+  // Push HomeScreen
+  HomeScreen = lv_obj_create(NULL);
+  lv_obj_set_pos(HomeScreen, 0, 0);
+  lv_obj_set_size(HomeScreen, ScreenWidth, ScreenHeight);
+  lv_obj_add_event_cb(HomeScreen, cbEventHandler, LV_EVENT_ALL, NULL);
+  lv_scr_load(HomeScreen);
+}
+
+void loop(){
   lv_task_handler();
-  delay(5);
+  delay(1);
 }
