05824ceb54d115056ef9afa98cc65fd5edab7b44
Gregory Sizer <gsizer@gmail.com>
2022-01-19 21:15:07 -0700

    functional buttons, ghost button as well

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,6 @@
 // System Constants
 const uint16_t ScreenWidth = 320;
 const uint16_t ScreenHeight = 480;
-enum MachineStates { Boot, Wait, Run, Error };
 
 //********************************
 // Global Objects
@@ -25,29 +24,33 @@ TouchPoint LastTouch;
 
 //********************************
 // Global Variables
-MachineStates CurrentState;
 uint32_t TouchCount = 0;
 
 //********************************
 // BUTTON_H depends on Display and TouchPanel existing
 // no error checking yet
 class button{
+  //********************************
   private:
     uint8_t _x, _y, _w, _h;
-    char *_label;
+    const char *_label;
     void(*_callback)()=nullptr;
+    //********
     void _press(){
-      Display.fillRoundRect(_x, _y, _w, _h, 4, TFT_CYAN);
-      Display.setTextColor(TFT_ORANGE);
+      Display.drawRoundRect(_x, _y, _w, _h, 4, TFT_CYAN);
+      Display.setTextColor(TFT_BLACK);
       Display.drawCentreString(_label, _x+(_w/2), _y+(_h/2), 4);
     }
+    //********
     void _release(){
       Display.fillRoundRect(_x, _y, _w, _h, 4, TFT_DARKCYAN);
-      Display.setTextColor(TFT_WHITE);
+      Display.setTextColor(TFT_BLACK);
       Display.drawCentreString(_label, _x+(_w/2), _y+(_h/2), 4);
     }
+  //********************************
   public:
-    button(uint8_t x, uint8_t y, uint8_t w, uint8_t h, char *l, void(*callback)()){
+    //********
+    button(uint8_t x, uint8_t y, uint8_t w, uint8_t h, const char *l, void(*callback)()){
       _x=x;
       _y=y;
       _w=w;
@@ -56,27 +59,29 @@ class button{
       _callback=callback;
       _release();
     }
+    //********
     void checkTouch(uint8_t x, uint8_t y){
-      if(x>_x && x<_w){
+      if(x>_x && x<(_x+_w)){
         // button laterally hit
-        if( y>_y && y<_h){
+        if( y>_y && y<(_y+_h)){
           // button vertically hit
           // execute code at callback
           _press();
           (*_callback)();
-          _release();
         }
       }
+      _release();
     }
 };
 
 //********************************
-// put callbacks above button declarations so the address
+// put callback functions above button declarations so the address
 // is in memory when being targeted as a callback
 
 //********************************
 // logPress
 static void logPress(){
+  TouchCount++;
   if(Serial.availableForWrite()){
     Serial.printf("Touch %u at %u:%u\n", TouchCount, LastTouch.xPos, LastTouch.yPos);
   }
@@ -84,13 +89,14 @@ static void logPress(){
 
 //********************************
 // Screen Objects
-button btnBTLE = button(10, 10, 64, 64, "BTLE", &logPress);
-button btnWIFI = button(10, 84, 64, 64, "WIFI", &logPress);
-button btnHOME = button(10, 168, 64, 64, "HOME", &logPress);
+const char *labels[3] = {"BTLE", "WIFI", "HOME"};
+
+button btnBTLE = button(10, 10, 64, 64, labels[0], &logPress);
+button btnWIFI = button(10, 84, 64, 64, labels[1], &logPress);
+button btnHOME = button(10, 158, 64, 64, labels[2], &logPress);
 
 //********************************
 void setup(){
-  CurrentState = Boot;
   // Hardware Display
   Display.init();
   Display.setRotation(1);
@@ -109,26 +115,10 @@ void setup(){
 //********************************
 void loop(){
   LastTouch = TouchPanel.read();
-  switch (CurrentState){
-    case Boot:
-      // load settings
-      break;
-    case Wait:
-      // power saving
-      break;
-    case Run:
-      // execute application
-      if(LastTouch.touched){
-        btnBTLE.checkTouch(LastTouch.xPos, LastTouch.yPos);
-        btnWIFI.checkTouch(LastTouch.xPos, LastTouch.yPos);
-        btnHOME.checkTouch(LastTouch.xPos, LastTouch.yPos);
-      }
-      break;
-    case Error:
-      // cosmic entity
-    default:
-      // unlikely default, reboot both cpu
-      esp_restart();
-      break;
+  if(LastTouch.touched){
+    btnBTLE.checkTouch(LastTouch.xPos, LastTouch.yPos);
+    btnWIFI.checkTouch(LastTouch.xPos, LastTouch.yPos);
+    btnHOME.checkTouch(LastTouch.xPos, LastTouch.yPos);
   }
+  delay(100);
 }
